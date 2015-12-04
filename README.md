a328b5be8e00dfe7be9c5205e9ee19a8b30ad9a6
Rutvi Patel <rutvi@uw.edu>
2015-12-03 16:30:24 -0800

    ade contribution repo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# walkwithme
+# final-project
+This is our final project for INFO 343 at University of Washington.
+
+Github Link: https://github.com/rutvimpatel/final-project.git
---
+++ js/walk.js
@@ -0,0 +1,27 @@
+(function() {
+
+	var myApp = angular.module('myApp', ['firebase']);
+	myApp.controller('myCtrl', function($scope, $firebaseAuth, $firebaseObject, $http){ // Do we need http?
+		console.log('weeeeeeee');
+		
+		var ref = new Firebase('https://walkwithme343.firebaseio.com/');
+
+		var usersRef = ref.child("users");
+
+		// firebaseObject of users
+		$scope.users = $firebaseObject(usersRef);
+		$scope.authObj = $firebaseAuth(ref);
+
+		// Test if already logged in
+		var authData = $scope.authObj.$getAuth();
+		if (authData) {
+		  $scope.userID = authData.uid;
+		  console.log($scope.userID);
+		}
+
+		console.log(authData);
+
+	})
+
+
+})();
